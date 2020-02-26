939360f9d1bafa9019b6ff8739495c6c9101c4a1
David Smith <shmish111+github@gmail.com>
2020-02-26 17:39:40 +0300

    enable injection of object to grammar

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const compile = require('nearley/lib/compile')
 const generate = require('nearley/lib/generate')
 const nearleyGrammar = require('nearley/lib/nearley-language-bootstrapped')
 
-function compileGrammar (sourceCode, callback) {
+function compileGrammar(sourceCode, callback) {
   // Parse the grammar source into an AST
   const grammarParser = new nearley.Parser(nearleyGrammar)
   grammarParser.feed(sourceCode)
@@ -13,11 +13,13 @@ function compileGrammar (sourceCode, callback) {
   // Compile the AST into a set of rules
   const grammarInfoObject = compile(grammarAst, {})
   // Generate JavaScript code from the rules
-  const output = generate(grammarInfoObject, 'grammar')
+  // Note that we hack this so that grammar is a function that we can pass something to.
+  // This enables us to inject things into the grammar and reference them using the 'opts' value
+  const output = generate(grammarInfoObject, 'grammar').replace('var grammar = {', 'var grammar = function(opts) { return {').replace('if (typeof module', '}; if (typeof module');
   callback(null, output)
 }
 
-function replacePaths (source, baseDir) {
+function replacePaths(source, baseDir) {
   return source.replace(/@include[\s]+"([^"]+)"/gm, function (match, m1) {
     return '@include "' + baseDir + m1 + '"'
   })
